441c487e0cd3622572e32a1fb622d7b42f781172
samhooper256 <samhooper256@gmail.com>
2022-03-17 12:22:55 -0500

    Updated to-do list.

--- tagmare/src/base/Todo.java
+++ tagmare/src/base/Todo.java
@@ -10,13 +10,13 @@ public final class Todo {
 	 * - Add card rarities?
 	 * - Make "View Deck" button look nicer (maybe make 2 drawings in Google Drawings, one with a dropshadow for when
 	 * you hover over it)?
-	 * 
+	 *  - Buff IntentPart artwork
+	 *  - Debuff IntentPart artwork
 	 * - Modifier Icon artwork
 	 * - Show your health on the calendar page.
 	 * - Show what the combats are on the calendar page.
 	 * - Finish calendar artwork (add Week 1, Week 2, etc. text)
 	 * - Add desk artwork.
-	 * - Some sort of "hop" animation on PlayerModifierIcons to show when they're doing stuff? (e.g. 
 	 * - ENEMIES:
 	 * 		- Ideas!
 	 * 		- More intents (debuff, buff, attack+buff, attack with multiple strikes) 
@@ -24,6 +24,7 @@ public final class Todo {
 	 * 
 	 * OPTIONAL:
 	 * - Make EOTLoseBlock smoothly fade out and slide down like SOTLoseBlock does for player.
+	 * - Some sort of "hop" animation on PlayerModifierIcons to show when they're doing stuff?
 	 */
 	
 	private Todo() {
